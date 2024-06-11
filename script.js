b0d1decfb1bb6e69fa2f6ec365afc83f28793f03
SamLiao888 <samliao888@gmail.com>
2024-06-11 15:36:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,28 @@ let lastY = 0;
 
 function startDrawing(e) {
 isDrawing = true;
-[lastX, lastY] = [e.offsetX, e.offsetY];
+[lastX, lastY] = [e.touches[0].clientX - canvas.offsetLeft, e.touches[0].clientY - canvas.offsetTop];
 }
 
 function draw(e) {
 if (!isDrawing) return;
+e.preventDefault(); // 防止滾動
 ctx.beginPath();
 ctx.moveTo(lastX, lastY);
-ctx.lineTo(e.offsetX, e.offsetY);
+ctx.lineTo(e.touches[0].clientX - canvas.offsetLeft, e.touches[0].clientY - canvas.offsetTop);
 ctx.stroke();
-[lastX, lastY] = [e.offsetX, e.offsetY];
+[lastX, lastY] = [e.touches[0].clientX - canvas.offsetLeft, e.touches[0].clientY - canvas.offsetTop];
 }
 
 function stopDrawing() {
 isDrawing = false;
 }
 
+canvas.addEventListener('touchstart', startDrawing);
+canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('touchend', stopDrawing);
+
+// 也保留滑鼠事件，以便在支持滑鼠的設備上使用
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stopDrawing);
